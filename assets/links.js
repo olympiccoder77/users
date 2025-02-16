43c880cdbe22629e19549c957ee0d3f484009ee6
olympiccoder77 <olympicmillionaire&@gmail.com>
2025-02-16 22:05:58 +0200

    users

---
+++ assets/links.js
@@ -0,0 +1,40 @@
+// لينك متجر الشركاء
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "https://www.blogger.com/";  // اللينك
+    document.getElementById("partnersstore").href = link;
+});
+
+// لينك متجر pronox
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "#";  // اللينك
+    document.getElementById("pronoxstoreLink").href = link;
+});
+
+// لينك طلب المنتجات
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "3";  // اللينك
+    document.getElementById("orderform").href = link;
+});
+// لينك سحب الارباح
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "https://olympiccoder77.github.io/pronox-forms/";  // اللينك
+    document.getElementById("withdraw").href = link;
+});
+
+// facebook
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "https://www.facebook.com/profile.php?id=61570083616232";  // اللينك
+    document.getElementById("facebooklink").href = link;
+});
+
+// instagram
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "https://www.instagram.com/pronox.7";  // اللينك
+    document.getElementById("instagramlink").href = link;
+});
+
+// telegram
+document.addEventListener("DOMContentLoaded", () => {
+    const link = "https://t.me/Pronox7";  // اللينك
+    document.getElementById("telegramlink").href = link;
+});
